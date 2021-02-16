167b8ad736c5297f8da947f009f3b603955ede65
ELKOYET <andrey_strelkov@mail.ru>
2021-02-16 15:55:05 +0500

    Upload

---
+++ Hahatun/Hahatun/Controllers/FileModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hahatun.Controllers
+{
+    public class FileModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+    }
+}
---
+++ Hahatun/Hahatun/Controllers/UploadFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Hahatun.Controllers   
+{
+    public class UploadFile : Controller
+    {
+        IWebHostEnvironment _appEnvironment;
+        public UploadFile(IWebHostEnvironment appEnvironment)
+        {
+            
+            _appEnvironment = appEnvironment;
+        }
+
+      
+
+        [HttpPost]
+        public IActionResult LoadFile(IFormFile picture)
+        {
+            if (picture != null)
+            {
+                string path = "/Files/" + picture.FileName;
+                using (var filestream = new FileStream(_appEnvironment.WebRootPath + path, FileMode.Create))
+                {
+                    picture.CopyToAsync(filestream);
+                }
+            }
+
+                return View();
+        }
+
+        
+       
+    }
+}
